0cb23afc3fb0b3f08079425be9441d956ff31908
Shashi Dhar Kumar <11044107+shashidharkumar@users.noreply.github.com>
2024-08-18 16:11:48 +0530

    Update SpeakHello.js

--- HTML-CSS-Javascript/module4-solution/SpeakHello.js
+++ HTML-CSS-Javascript/module4-solution/SpeakHello.js
@@ -6,7 +6,7 @@
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 // var helloSpeaker =
-var helloSpeaker = (names);
+var helloSpeaker = {};
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
@@ -15,8 +15,8 @@ var speakWord = "Hello";
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 
-helloSpeaker.speak = function () {
-  console.log(speakWord + " " + names);
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
 }
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
